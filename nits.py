998db82536c2077dbdb157aa21af8c6b84957761
Farhad-Mrkm <79039791+Farhad-Mrkm@users.noreply.github.com>
2022-02-20 13:24:19 -0800

    Add files via upload

--- nits.py
+++ nits.py
@@ -54,18 +54,8 @@ class NIT(nn.Module):
       self.peak_activation = PeakConstraint(peak)
 
   def forward(self, x):
-    if self.chan_type=='discrt_rician' :
-      batch_size = x.size(0)
-      unnorm_tx = self._f(x)
-
-      norm_tx = unnorm_tx/torch.sqrt(torch.mean(torch.pow(unnorm_tx,2.0)))*torch.sqrt(self.avg_P)
-      norm_tx=(norm_tx/torch.pow(torch.mean(torch.pow(norm_tx,4.0)),.25))*torch.sqrt(self.avg_P)*torch.tanh(norm_tx)
-
-      if self.peak is not None:
-        norm_tx = self.peak_activation(norm_tx)
-
-      return norm_tx
-    elif self.chan_type=='conts_awgn':
+   
+    if self.chan_type=='conts_awgn':
         unnorm_tx = self._f(x)
 
         norm_tx = unnorm_tx/torch.sqrt(torch.mean(torch.pow(unnorm_tx,2.0)))*torch.sqrt(self.avg_P)
@@ -78,19 +68,7 @@ class NIT(nn.Module):
 
         return norm_tx
     
-    elif self.chan_type=='discrt_poisson':
-      batch_size = x.size(0)
-      unnorm_tx = self._f(x)
-   # norm_tx = unnorm_tx/torch.sqrt(torch.mean(torch.pow(unnorm_tx,2.0)))*torch.sqrt(self.avg_P)
-      norm_tx=(unnorm_tx/(torch.mean(unnorm_tx)))*(torch.sqrt(self.avg_P))
-    #norm_tx=torch.tanh((unnorm_tx))*torch.sqrt(self.avg_P)
-    #if self.channel == 'awgn':
-      #norm_tx = unnorm_tx/torch.sqrt(torch.mean(torch.pow(unnorm_tx,2.0)))*torch.sqrt(self.avg_P)
-    #elif self.channel == 'poisson':
-      #unnorm_tx = torch.log10(torch.cosh(unnorm_tx))
-      #unnorm_tx = torch.absolute(unnorm_tx)
-      #norm_tx = unnorm_tx/torch.mean(unnorm_tx)*self.avg_P
-      #norm_tx = torch.log10(torch.cosh(unnorm_tx))
+    
     
       if self.positive is not None:
         norm_tx=(torch.cosh(norm_tx))-1.0
@@ -105,15 +83,6 @@ class NIT(nn.Module):
         return norm_tx
       
 
-#class AWGN_Chan(nn.Module):
-  #"""AWGN Channel """
-  #def __init__(self, stdev=1.0):
-   # super(AWGN_Chan, self).__init__()
-    #self.stdev = torch.tensor(stdev)
-
-  #def forward(self, x):
-   # noise = torch.randn_like(x) * self.stdev
-   # return x + noise
 class _Channel(nn.Module):
   """AWGN Channel """
   def __init__(self,type1):
@@ -121,25 +90,9 @@ class _Channel(nn.Module):
     self.stdev = torch.tensor(1.0,dtype=torch.float)
     self.type1=type1
     ##
-    self.dark_current =0#torch.tensor(params)
-    self.delta0=10
-    #self.stdev = torch.tensor(params/10000)
-      #self.delta=torch.tensor(0.3839)
-    self.delta=torch.tensor(1.0)
   def forward(self, x):
-    if self.type1=='discrt_rician':
-       noise = torch.randn_like(x) * self.stdev
-      #fc=torch.randn_like(torch.transpose(x,0,1),dtype=torch.cfloat)
-       fc=torch.randn_like(x)*(self.stdev)
-       fc1=torch.ones_like(x)*.5
-       fmid = torch.mul(fc,x)
-       fmid1=torch.mul(fc1,x)
-       y=fmid[:,0]-fmid[:,1]+noise[:,0]+fmid1[:,0]
-       return y
-    elif self.type1=='conts_awgn':
+   
+    if self.type1=='conts_awgn':
        noise = torch.randn_like(x) * self.stdev
        return x + noise
-    elif self.type1=='discrt_poisson':
-      y = torch.poisson(x*self.delta+self.dark_current*self.delta0)
-      noise = torch.randn_like(x) * self.stdev
-      return y#+noise
+   
