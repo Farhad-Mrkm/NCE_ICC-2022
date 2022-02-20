001dc9a8afb6780c9627ec69db302ac506488594
Farhad-Mrkm <79039791+Farhad-Mrkm@users.noreply.github.com>
2022-02-20 13:23:13 -0800

    Add files via upload

--- joint_training.py
+++ joint_training.py
@@ -1,4 +1,4 @@
-# author__Farhad_Mirkarimi!/usr/bin/env python
+﻿# author__Farhad_Mirkarimi!/usr/bin/env python
 # coding: utf-8
 
 # In[ ]:
@@ -74,27 +74,8 @@ def joint_training(typeinp,nit_params,critic_params,SNR,estimator='mine',init_ep
 
     estimates = np.zeros(init_epoch)
     for e in tqdm(range(init_epoch)):
-      if typeinp=='discrt_rician':
-        seed_rv = np.random.randint(0,seed_size,(batch_size,2))*seed_rv_std
-        batch_s = torch.tensor(seed_rv, dtype=torch.float).to(device) 
-        batch_x = nit(batch_s)
-        batch_y = channel(batch_x)
-        zer=torch.ones_like(batch_y)*.001
-        zer1=zer.view(batch_size,1)
-        batchy1=batch_y.view(batch_size,1)
-        fd=torch.cat((batchy1,zer1),1)
-        t_xy = mi_neuralnet(batch_x, fd)
-        if estimator=='mine':
-            mi_est = mi_est_loss.mi_est_loss(t_xy)
-        elif estimator=='smile':
-            mi_est=mi_est_loss.mi_est_loss(t_xy,clip=clip)
-        loss = -mi_est
-        loss.backward()
-        #torch.nn.utils.clip_grad_norm_(mi_neuralnet.parameters(), 0.2)
-        opt_mi.step()
-        mi_est = mi_est.detach().cpu().numpy()
-        estimates[e]= mi_est
-      elif typeinp=='conts_awgn':
+      
+      if typeinp=='conts_awgn':
         seed_rv = np.random.randn(batch_size,1)*seed_rv_std
         batch_s = torch.tensor(seed_rv, dtype=torch.float).to(device) 
         batch_x = nit(batch_s)
@@ -114,38 +95,7 @@ def joint_training(typeinp,nit_params,critic_params,SNR,estimator='mine',init_ep
         opt_mi.step()
         mi_est = mi_est.detach().cpu().numpy()
         estimates[e]= mi_est
-      elif typeinp=='discrt_poisson':
-        clip=None
-        opt_nit.zero_grad()
-        opt_mi.zero_grad()
-    ##
-        seed_rv0 = np.random.randn(batch_size,1)*.001
-   # seed_rv =np.reshape(np.random.poisson(1,batch_size),(batch_size,1))+seed_rv0
-        seed_rv=np.random.randint(0,seed_size,size=(batch_size,1))*50
-    #seed_rv=np.random.uniform(0,nit_params['peak_amp'],(batch_size,1))*50+seed_rv0
-
-        batch_s = torch.tensor(seed_rv, dtype=torch.float).to(device) 
-        batch_x = nit(batch_s)
-    # if e==0:
-    #   print(batch_x)
-        batch_y = channel(batch_x)
-    # if e==0:
-    #   print(batch_x)
-    #   print(batch_y)
-        t_xy = mi_neuralnet(batch_x, batch_y) ##
-    # if e==0:
-    #   print(t_xy)
-        if estimator=='mine':
-            mi_est = mi_est_loss.mi_est_loss(t_xy)
-        elif estimator=='smile':
-            mi_est=mi_est_loss.mi_est_loss(t_xy,clip=clip)
-       
-        loss = -mi_est
-        loss.backward()
-    #torch.nn.utils.clip_grad_norm_(mi_neuralnet.parameters(), 0.2)
-        opt_mi.step()
-        mi_est = mi_est.detach().cpu().numpy()
-        estimates[e]= mi_est
+     
 
     plt.plot(estimates)
     plt.show()
@@ -169,28 +119,8 @@ def joint_training(typeinp,nit_params,critic_params,SNR,estimator='mine',init_ep
         ###loss = -mi_est
         ###loss.backward()
       #torch.nn.utils.clip_grad_norm_(nit.parameters(), 0.2)
-        if typeinp=='discrt_rician':
-          seed_rv = np.random.randint(0,seed_size,(batch_size,2))*seed_rv_std
-          batch_s = torch.tensor(seed_rv, dtype=torch.float).to(device) 
-          batch_x = nit(batch_s)
-          batch_y = channel(batch_x)
-          zer=torch.ones_like(batch_y)*.001
-          zer1=zer.view(batch_size,1)
-          batchy1=batch_y.view(batch_size,1)
-          fd=torch.cat((batchy1,zer1),1)
-          t_xy = mi_neuralnet(batch_x, fd)
-          if estimator=='mine':
-            mi_est = mi_est_loss.mi_est_loss(t_xy)
-          elif estimator=='smile':
-            mi_est=mi_est_loss.mi_est_loss(t_xy,clip=clip)
         
-          loss = -mi_est
-          loss.backward()
-          #torch.nn.utils.clip_grad_norm_(mi_neuralnet.parameters(), 0.2)
-          opt_nit.step()
-          #mi_est = mi_est.detach().cpu().numpy()
-          #estimates[e]= mi_est
-        elif typeinp=='conts_awgn':
+        if typeinp=='conts_awgn':
           seed_rv = np.random.randn(batch_size,1)*seed_rv_std
           batch_s = torch.tensor(seed_rv, dtype=torch.float).to(device) 
           batch_x = nit(batch_s)
@@ -279,33 +209,6 @@ def joint_training(typeinp,nit_params,critic_params,SNR,estimator='mine',init_ep
 
           estimates[e]= mi_est
       ##
-        elif typeinp=='discrt_poisson':
-          clip=2
-          opt_nit.zero_grad()
-          opt_mi.zero_grad()
-          seed_rv0 = np.random.randn(batch_size,1)*.001
-        #seed_rv = np.reshape(np.random.poisson(1,batch_size),(batch_size,1))+seed_rv0
-          seed_rv=np.random.randint(0,seed_size,size=(batch_size,1))*50
-        #seed_rv=np.random.uniform(0,nit_params['peak_amp'],(batch_size,1))*50+seed_rv0
-          batch_s = torch.tensor(seed_rv, dtype=torch.float).to(device) 
-          batch_x = nit(batch_s)
-          batch_y = channel(batch_x)
-          t_xy = mi_neuralnet(batch_x, batch_y)
-          if estimator=='mine':
-            mi_est = mi_est_loss.mi_est_loss(t_xy)
-          elif estimator=='smile':
-            mi_est=mi_est_loss.mi_est_loss(t_xy,clip=clip)
-        
-          loss = -mi_est
-          loss.backward()
-        ##
-            
-        #torch.nn.utils.clip_grad_norm_(mi_neuralnet.parameters(), 0.2)
-          opt_mi.step()
-          mi_est = mi_est.detach().cpu().numpy()
-
-          estimates[e]= mi_est
-            
         
           
 
@@ -318,31 +221,8 @@ def joint_training(typeinp,nit_params,critic_params,SNR,estimator='mine',init_ep
     
 
   ##########new function
-    if typeinp=='discrt_rician':
-      seed_rv = np.random.randint(0,seed_size,(batch_size,2))*seed_rv_std
-      batch_s = torch.tensor(seed_rv, dtype=torch.float).to(device) 
-      batch_x = nit(batch_s)
-      
-      batch_x = batch_x.detach().cpu().numpy()
-      print("The variance of batch is {}".format(np.var(batch_x)))
-      print("-Estimated Capacity {:.4f}".format(np.mean(estimates[-50:])))
-      plt.hist(batch_x, 100)
-      plt.show()
-      #from google.colab import files
-      plt.savefig("optm_inp_rician.png")
-      
-      est_cap.append(np.mean(estimates[-50:]))
-      plt.plot(all_snr,actual_cap, '-k', label='Actual Capacity')
-      plt.plot(all_snr,est_cap, '--r', label='MINE EST Capacity')
-      plt.legend()
-      plt.xlabel('SNR (dB)')
-      plt.ylabel('Capacity (nats)')
-      plt.show()
-      f = open("summary.txt", "a")
-      f.write("{:100s} channel capacity (nats) at snr,{:02d},{:2.5f}\n".format(typeinp,snr_db, np.mean(estimates[-50:])))
-      f.close()
-      #return batch_x,np.mean(estimates[-50:])
-    elif typeinp=='conts_awgn':
+   
+    if typeinp=='conts_awgn':
       seed_rv = np.random.randn(10000,1)*seed_rv_std
       batch_s = torch.tensor(seed_rv, dtype=torch.float).to(device) 
       batch_x = nit(batch_s)
@@ -360,31 +240,6 @@ def joint_training(typeinp,nit_params,critic_params,SNR,estimator='mine',init_ep
       f.close()
       #return batch_x,np.mean(estimates[-50:])
 
-    elif typeinp=='discrt_poisson':
-      all_batchx=[]
-      for kk in tqdm(range(250)): 
-      #seed_rv = np.random.randn(10000,1)*seed_rv_std
-        
-      # seed_rv = np.reshape(np.random.poisson(1,batch_size),(batch_size,1))+seed_rv0
-        seed_rv=np.random.randint(0,seed_size,size=(batch_size,1))*50
-      
-        batch_s = torch.tensor(seed_rv, dtype=torch.float).to(device) 
-        batch_x = nit(batch_s)
-      all_batchx.append(batch_x.detach().cpu().numpy())
-    
-      input_x_smaples = np.concatenate(all_batchx, axis=None)
-      print('The TX power at SNR {} dB is {}'.format(snr_db, nit_params['tx_power']))
-      print("The variance of batch is {}".format(np.var(input_x_smaples)))
-      print("-Estimated Capacity {:.4f}".format(np.mean(estimates[-50:])))
-      fig, axs = plt.subplots(1, 1, sharey=True, tight_layout=True)
-      axs.hist(input_x_smaples, bins=50)
-      plt.title("{}: sample size: {:d}, mean: {:2.3f} std: {:2.3f}".format('DV', input_x_smaples.shape[0], np.mean(input_x_smaples), np.std(input_x_smaples)))
-      plt.xlabel('alphabet')
-      plt.ylabel('density')
-      plt.show()
-      plt.savefig(optm-inp-poisson.png)
-      f = open("summary.txt", "a")
-      f.write("{:100s} channel capacity at snr,{:02d},{:2.5f}\n".format(typeinp,snr_db, np.mean(estimates[-50:])))
-      f.close()
+   
   return batch_x,np.mean(estimates[-50:])
 
